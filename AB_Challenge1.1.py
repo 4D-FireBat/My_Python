22e95507da552254381b7e47ffb5eb552d367aab
Randy <98669920+4D-FireBat@users.noreply.github.com>
2024-01-26 21:59:10 -0500

    Challenge 1.1 is all done in interactive mode. I've recorded the commands here.

---
+++ AB_Challenge1.1.py
@@ -0,0 +1,7 @@
+# AB Challenge 1.1
+# Create an erro of your very own by entering your favorite ice cream flavor in interactive mode.
+# Then, make up for your misdeed and enter a statement that prints the name of your favorite ice cream.
+
+# >>>Vanilla Bean
+
+# >>>print("Vanilla Bean")

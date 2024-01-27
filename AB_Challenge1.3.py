61f3612f9e5b82ccfe4d3e5e985b56b949a19591
Randy <98669920+4D-FireBat@users.noreply.github.com>
2024-01-26 22:17:29 -0500

    Added a description of the challenge and a line of code that displays my favorite quote on one line and credits the origin on another line.

---
+++ AB_Challenge1.3.py
@@ -0,0 +1,4 @@
+# AB Challenge 1.3
+# Write a program that prints your favorite quote. It should give credit to the person who said it on the next line
+
+input("\n 'Fear Profits A Man Nothing'\n ~Herger the Joyous - 13th Warrior")

466217b198bbf861db97b1349604fbde430074aa
Randy <98669920+4D-FireBat@users.noreply.github.com>
2024-01-27 16:28:21 -0500

    Created the shell for AB Challenge 2.2 and entered the challenge description.

---
+++ AB_Challenge2.2.py
@@ -0,0 +1,3 @@
+# AB Challenge 2.2
+# Write a program that allows a user to enter his or her two favorite foods. 
+# The program should then print out the name of a new food by joining the original food names together.

94559d09e34302b831c126bae717904ca849a4bf
Randy <98669920+4D-FireBat@users.noreply.github.com>
2024-01-26 22:06:12 -0500

    Added a description that explains the challenge then added a single line of code that prints my name and waits for the user to press enter.

---
+++ AB_Challenge1.2.py
@@ -0,0 +1,4 @@
+# AB Challenge 1.2
+# Write and save a program that prints out your name and waits for the user to press the Enter key before the program ends.
+
+input("\nKrennish\n\nPress the Enter key to exit.")

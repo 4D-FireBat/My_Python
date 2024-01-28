45f2c16a046daf3ffc12aaae71c353e2d2f98f09
Randy <98669920+4D-FireBat@users.noreply.github.com>
2024-01-28 01:00:37 -0500

    Added the description of the challenge and a line of code that asks for the inital price. Need to research dealer fees and what is and isn't taxable.

---
+++ AB_Challenge2.4.py
@@ -0,0 +1,11 @@
+# AB Challenge 2.4
+# Write a Car Salesman program where the user enters the base price of a car.
+# The program should add on a bumcn of extra fees such as tax, license, dealer prep, and destination charge.
+# Make tax and license a percent of the base price. The other fees should be set values.
+# Display the actual price of the car once all the extras are applied.
+
+# Title!
+
+print("\n\t\t\tThe Car Salesman!")
+
+base_price = float(input("\nPlease enter the base price of this vehicle and I will provide you with the final dealer price: "))

5acd51694bf2f7283410780df9c4c99e4997f700
Randy <98669920+4D-FireBat@users.noreply.github.com>
2024-01-27 23:18:37 -0500

    Added code that takes two inputs from the user and combines them into one display.

--- AB_Challenge2.2.py
+++ AB_Challenge2.2.py
@@ -1,3 +1,10 @@
 # AB Challenge 2.2
 # Write a program that allows a user to enter his or her two favorite foods. 
 # The program should then print out the name of a new food by joining the original food names together.
+
+food_1 = input("\nWhat is your favorite food? ")
+food_2 = input("\nAnd what is your second favorite food? ")
+
+print("\nHow about", food_1, food_2, "?")
+
+input("\n\nPress the Enter key to exit.")

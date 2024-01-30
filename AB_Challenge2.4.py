2e030a29c2795fd3d6afed3b33361b6bf9424cce
Randy <98669920+4D-FireBat@users.noreply.github.com>
2024-01-29 21:24:10 -0500

    Added some code that takes a base number provided by the user adds taxes calculated off the base price, and 'dealer fees' and presents the user with the total final price.

--- AB_Challenge2.4.py
+++ AB_Challenge2.4.py
@@ -1,6 +1,6 @@
 # AB Challenge 2.4
 # Write a Car Salesman program where the user enters the base price of a car.
-# The program should add on a bumcn of extra fees such as tax, license, dealer prep, and destination charge.
+# The program should add on a buncn of extra fees such as tax, license, dealer prep, and destination charge.
 # Make tax and license a percent of the base price. The other fees should be set values.
 # Display the actual price of the car once all the extras are applied.
 
@@ -9,3 +9,13 @@
 print("\n\t\t\tThe Car Salesman!")
 
 base_price = float(input("\nPlease enter the base price of this vehicle and I will provide you with the final dealer price: "))
+tax = base_price * 0.07
+license = base_price * 0.04
+dealer_prep = 500
+destination = 1250
+
+total_price = round(base_price + license + dealer_prep + destination, 2)
+
+print("\nYour total price after taxes, license and dealer fees is: $",total_price)
+
+input("\n\nPlease press the Enter key to exit.")

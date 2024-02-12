4e9aa3af59b200fd45d0e7db1d2611f7709b636b
Randy <98669920+4D-FireBat@users.noreply.github.com>
2024-02-12 18:48:16 -0500

    Initial commit with the challenge guidlines.

---
+++ LL_Files.py
@@ -0,0 +1,3 @@
+# Linkedin Learning Files Challenge
+# Calculate and return the total number of bytes of the text file sizes within the current working directory.
+# Only include text files that end with ".txt" in your calculation. Other files should be ignored

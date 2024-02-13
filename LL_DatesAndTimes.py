a0ffac192196aa7e6ea15a2e466bc50b0b7b9192
Randy <98669920+4D-FireBat@users.noreply.github.com>
2024-02-12 23:19:04 -0500

    Initial commit with the challenge guidelines in the file.

---
+++ LL_DatesAndTimes.py
@@ -0,0 +1,3 @@
+# Linkedin Learning Challenge Dates and Times
+# You're given a month and year, along with a day of the week (Monday through Sunday).
+# Return the number of instances of that day in the given month

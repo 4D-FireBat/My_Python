b286fdbcaf9b6d30f9b5da7c48494349d96d80ae
Randy <98669920+4D-FireBat@users.noreply.github.com>
2024-02-26 23:37:40 -0500

    Added the rules for the player.

--- AB_Challenge3.4.py
+++ AB_Challenge3.4.py
@@ -4,4 +4,11 @@
 
 import random
 
+def guess_your_num():
+    print("\tWelcome to the Guess YOUR Number Game")
+    print("\nThink of a number between 1 and 100, and I will attempt to guess your number while keeping track of my guesses.")
+    print("Use 'Higher' or 'Lower' to guide the program and enter 'Correct' when I guess the your number.")
+    print("Type 'Exit' to leave the program.")
 
+if __name__ == "__main__":
+    guess_your_num()

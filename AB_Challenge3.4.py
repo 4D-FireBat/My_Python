66d2c5096b9488a80a797f637f0deccbcaf5f39f
Randy <98669920+4D-FireBat@users.noreply.github.com>
2024-02-26 23:27:17 -0500

    Created the shell of the program that lays out the guidelines of the challenge.

---
+++ AB_Challenge3.4.py
@@ -0,0 +1,7 @@
+# AB Challenge 3.4
+# Write a program that is the opposite of the "Guess My Number" program.
+# Let the user pick a number between 1 and 100, and then try to guess that number based on their input.
+
+import random
+
+

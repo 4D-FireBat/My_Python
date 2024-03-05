65b709d217a927b99ceff699006d1f8523cf43e5
Randy <98669920+4D-FireBat@users.noreply.github.com>
2024-03-04 19:58:00 -0500

    Built out the initial game. Still need to work on the scoring system.

---
+++ AB_Challenge4.3.py
@@ -0,0 +1,46 @@
+# AB Challenge 4.3 
+# Create a "Word Jumble" game where a list of words are each jumbled and presented to the player to decypher
+# Each word presented should be accompanied by a hint that the player can ask for.
+# Add a scoring system that rewards the player for solving the word jumble without asking for the hint
+
+import random
+
+def word_jumble():
+    # Set a constant list of words to use
+    WORDS = ("python", "jumble", "easy", "difficult", "answer", "xylophone")
+    # Pick a word from the list at random and set it to the variable of correct to be checked against later
+    word = random.choice(WORDS)
+    correct = word
+    jumble = ""
+
+    while word:
+        # While there are still letters in the word, use slicing to remove the letters at random and add them to the jumble variable
+        position = random.randrange(len(word))
+        jumble += word[position]
+        word = word[:position] + word[(position + 1):]
+
+    # Start the game
+    print(
+    """
+                    Welcome to Word Jumble!
+                    
+            Unscramble the letters to make a word
+        (Press the Enter key at the prompt to quit.)
+        (Enter ? for a hint)
+    """
+    )
+    print("The jumble is:", jumble)
+    guess = input("\nYour guess: ")
+    while guess != correct and guess != "":
+        if guess == "?":
+            print("The first letter of this word is", correct[0])
+            guess = input("Your guess: ")
+        
+    if guess == correct:
+        print("That's it! You guessed it\n")
+        print("Thanks for playing!")
+        input("\n\nPress the Enter key to exit")
+
+
+if __name__ == "__main__":
+    word_jumble()

90704b29560edd0489a9ab6f17ac90be396ad9a2
Randy <98669920+4D-FireBat@users.noreply.github.com>
2024-04-13 00:51:49 -0400

    Added code for the user to search by son to find the father, add a new father/son combo, and exit the program.

--- AB_Challenge5.3.py
+++ AB_Challenge5.3.py
@@ -12,14 +12,57 @@ def daddy():
                "Nemo":"Marlin"}
     run = True
     while run:
-        son = input("Who's father are you looking for?")
-        soncap = son.title()
-        if soncap in fathers:
-            print("I have the father of ", soncap, " listed as: ", fathers.get(soncap))
-        else:
-            print("I don't have a father listed for ", soncap)
-            print("\nWould you like to add them?")
-            add = input("Yes or No?")
+
+        print("\tWho's Your Daddy?")
+        print("""
+        1. Look up a Father
+        2. Add a new Father/Son Combo
+        3. Exit """)
+
+        option = input("\nWhat would you like to do? ")
+        
+        if option == "1":
+            son = input("\nWho's father are you looking for? ")
+            soncap = son.title()
+            if soncap in fathers:
+                print("I have the father of ", soncap, " listed as: ", fathers.get(soncap))
+            else:
+                print("I don't have a father listed for ", soncap)
+                print("\nWould you like to add them?")
+                add = input("Yes or No? ")
+                if add.title() == "Yes":
+                    newson = soncap.title()
+                    dad = input("Who is the father of " + str(newson) + "? ")
+                    newfather = dad.title()
+                    fathers[newson] = newfather
+                    print("\nI now have the father of ", newson, " listed as ", newfather, ".")
+                    input("Press the Enter key to continue.")
+        
+        if option == "2":
+            son = input("\nWho is the son that you would like to add? ")
+            newson = son.title()
+            if newson in fathers:
+                print("I currently have ", newson, "'s father listed as", fathers.get(newson))
+                print("Would you like to change this record?")
+                update = input("Yes or No?")
+                if update.title() == "Yes":
+                    truefather = input("Who is the true father of " + str(newson) + "? ")
+                    newfather = truefather.title()
+                    fathers.update(newson = newfather)
+                    print("I have updated my records to show that ", fathers.get(newson), " is the father of ", newson, ".")
+                    input("\nPress the Enter key to continue.")
+            else:
+                dad = input("And who would you like to enter as the father of " + newson + " ? ")
+                newfather = dad.title()
+                fathers[newson] = newfather
+                print("\nI now have the father of ", newson, " listed as ", newfather, ".")
+                input("Press the Enter key to continue.")
+
+
+        if option == "3":
+            input("Thanks for Playing!")
+            run = False
+
 
 
 
